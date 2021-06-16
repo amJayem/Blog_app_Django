539667b316eeaea2aeec1381fde796ed17f437dd
amJayem <csejayem19@gmail.com>
2021-06-16 18:18:54 +0600

    blog post update

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView,DetailView,DeleteView,CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView,DetailView,DeleteView,CreateView,UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 
 def home(request):
     context = {
@@ -25,6 +25,21 @@ class PostCreateView(LoginRequiredMixin ,CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+class PostUpdateView(UserPassesTestMixin,UpdateView):
+    model = Post
+    fields = ['title','content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 def about(request):
     return render(request,'blog/about.html')
