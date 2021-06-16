b6432ec480103d55900018194dae913aeed9ef60
amJayem <csejayem19@gmail.com>
2021-06-16 18:09:39 +0600

    create a blog post

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView,DetailView,DeleteView
+from django.views.generic import ListView,DetailView,DeleteView,CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def home(request):
     context = {
@@ -17,6 +18,13 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     
+class PostCreateView(LoginRequiredMixin ,CreateView):
+    model = Post
+    fields = ['title','content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     return render(request,'blog/about.html')
