2a17ee0a51ea13a21c5c2cf21f6bc1206554d804
amJayem <csejayem19@gmail.com>
2021-06-17 20:40:01 +0600

    pagination & user post count

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from .views import PostListView,PostDetailView,PostCreateView,PostUpdateView,PostDeleteView
+from .views import PostListView,PostDetailView,PostCreateView,PostUpdateView,PostDeleteView,UserPostListView
 
 urlpatterns = [
     # path('',views.home,name='blog_home'),
     path(''              ,PostListView.as_view(),name='blog_home'),
     path('post/<int:pk>/',PostDetailView.as_view(),name='post_detail'),
+    path('user/<str:username>/',UserPostListView.as_view(),name='user_posts'),
     path('post/new/',PostCreateView.as_view(),name='post_create'),
     path('post/<int:pk>/update/',PostUpdateView.as_view(),name='post_update'),
     path('post/<int:pk>/delete/',PostDeleteView.as_view(),name='post_delete'),
