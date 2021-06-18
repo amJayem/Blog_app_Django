eeffc4ebae8892d1130f66c771352ff39c253b2c
amJayem <csejayem19@gmail.com>
2021-06-18 17:47:17 +0600

    password reset email possible now

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -139,3 +139,12 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# smtp configuration (simple mail transfer protocol)
+ 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'email_address' # os.environ.get('email_address')
+EMAIL_HOST_PASSWORD = 'email_PASS' # os.environ.get('email_PASS')
