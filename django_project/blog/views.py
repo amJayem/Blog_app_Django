33a2fe91a82b12b975368a2809dd7f40412a317b
amJayem <csejayem19@gmail.com>
2021-07-10 18:19:24 +0600

    views.py

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -20,7 +20,7 @@ class PostListView(ListView):
 
 class UserPostListView(ListView):
     model = Post
-    template_name = 'blog/user_posts.html'
+    template_name = 'blog/user_posts.html' # <app>/<model>_<Listview>.html
     context_object_name = 'posts'
     paginate_by = 5
     
